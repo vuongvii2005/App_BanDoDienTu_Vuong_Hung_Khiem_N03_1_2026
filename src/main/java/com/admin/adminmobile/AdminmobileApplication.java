cacac7b7206710e2f64ca234c17ffd0410ca24a3
Nguyen Quang Hung <hungquang2005@gmail.com>
2026-05-26 10:58:33 +0700

    init project admin folder

---
+++ src/main/java/com/admin/adminmobile/AdminmobileApplication.java
@@ -0,0 +1,13 @@
+package com.admin.adminmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminmobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminmobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/admin/adminmobile/AdminmobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.adminmobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminmobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
